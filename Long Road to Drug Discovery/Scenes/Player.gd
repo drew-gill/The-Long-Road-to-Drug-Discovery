4e467dee3b7728299b1b9eb977757a2c760722b6
drew-gill <drewgill89@gmail.com>
2020-07-07 09:37:26 -0400

    Adding new animal sprites, multiplayer rat animated

---
+++ Long Road to Drug Discovery/Scenes/Player.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+export var money = 1000000
+export var years = 20
+export var backups = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
