7efa5e14b0647f9931ab6f3119e0a8587325af57
Tahmid Hasan <tahmidhasan1999@gmail.com>
2020-07-08 19:55:01 -0400

    Added Board, BoardTile, and Piece
    Board is the gameboard made from an array of BoardTiles.

---
+++ Long Road to Drug Discovery/Prefabs/Piece.gd
@@ -0,0 +1,29 @@
+extends Node2D
+class_name Piece
+
+#piece properties
+var moneySpent = 0
+var timeElapsed = 0 
+
+#Signals
+signal leaving_tile
+
+#access the board itself
+onready var board = get_node("/root/Node2D")
+
+func _ready():
+	pass
+
+#get the tile that the piece is currently on
+func get_current_tile()-> BoardTile:
+	#CHANGE DENOMINATOR TO SIZE OF SQUARE
+	var tile_position = position/board.TILE_WIDTH
+	tile_position.x = int(tile_position.x)
+	tile_position.y = int(tile_position.y)
+	return board.get_tile_by_pos(tile_position)
+	
+#When the piece is clicked:
+func _on_Button_pressed():
+	print(get_current_tile().get_tile_ID())
+	print("Money Spent: $" + str(moneySpent))
+	board.move_piece(self, Vector2(1,0))
