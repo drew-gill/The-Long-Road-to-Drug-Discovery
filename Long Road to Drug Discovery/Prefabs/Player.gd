e5fdf4b47019d24f0c6fd69b6290176c134eee2b
drew-gill <drewgill89@gmail.com>
2020-07-13 16:19:33 -0400

    Added support for player turns and multiple players

--- Long Road to Drug Discovery/Prefabs/Player.gd
+++ Long Road to Drug Discovery/Prefabs/Player.gd
@@ -3,15 +3,16 @@ extends KinematicBody2D
 var money = 1000000
 var years = 20
 var backups = 0
-export var playerNumber = 1
+export var playerNumber = 1 #Player 1 vs Player 2, for example
 
-export (int) var movementSpeed = 50
+export (int) var movementSpeed = 75
 var movementTarget = Vector2()
 var movementVelocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	setAnimation()
+	movementTarget = self.position
 	
 func setPlayerNumber(num):
 	playerNumber = num
@@ -38,15 +39,20 @@ func alterPlayerBackups(changeValue):
 	backups += changeValue
 
 func _input(event):
-	if event.is_action_pressed('click'):
-		movementTarget = get_global_mouse_position()
+	if event is InputEventMouseButton:
+		movementTarget = event.position
 	
 func _physics_process(delta):
-	movementVelocity = position.direction_to(movementTarget) * movementSpeed
-	look_at(movementTarget)
-	if(position.distance_to(movementTarget) > 5):
-		movementVelocity = move_and_slide(movementVelocity)
-		$MouseSprite.play()
+	if(playerNumber == $"../PlayerTracker".getCurrentPlayer()):
+		movementVelocity = position.direction_to(movementTarget) * movementSpeed
+		$MouseSprite.flip_h = movementVelocity.x > 0
+		
+		
+		if(position.distance_to(movementTarget) > 5):
+			movementVelocity = move_and_slide(movementVelocity)
+			$MouseSprite.play()
+		else:
+			$MouseSprite.stop()
 	else:
 		$MouseSprite.stop()
 
--- Long Road to Drug Discovery/Scripts/HUD.gd
+++ Long Road to Drug Discovery/Scripts/HUD.gd
@@ -1,10 +1,8 @@
 extends Node
 
-export var numOfPlayers = 1
-#up to player 4, if there are 4 total players
-var currentPlayer = 1 
 
-
+var currentPlayerNumber
+var currentPlayer
 
 func _ready():
 	_showAll(false)
@@ -45,21 +43,21 @@ func _showWarning(text):
 	$Warning.show()
 
 func _on_AddMoney_pressed():
-	$"../Player".alterPlayerMoney(1000)
+	currentPlayer.alterPlayerMoney(1000)
 
 func _on_SubtractMoney_pressed():
-	$"../Player".alterPlayerMoney(-1000)
+	currentPlayer.alterPlayerMoney(-1000)
 
 
 func _on_AddYear_pressed():
-	$"../Player".alterPlayerYears(1)
+	currentPlayer.alterPlayerYears(1)
 
 
 func _on_SubtractYear_pressed():
-	$"../Player".alterPlayerYears(-1)
+	currentPlayer.alterPlayerYears(-1)
 
 func _on_EndTurn_pressed():
-	_endTurn()
+	$"../PlayerTracker".endTurn()
 	
 
 func addCommas(value):
@@ -74,18 +72,18 @@ func addCommas(value):
 
 	return res
 
-func _endTurn():
-	if(currentPlayer + 1 > numOfPlayers):
-		currentPlayer = 1
-	else:
-		currentPlayer += 1
+
 
 func _process(delta):
-	if($"../Player" != null):
-		$Money.text = "Money: $" + addCommas($"../Player".getPlayerMoney())
-		$Years.text = "Years left: " + str($"../Player".getPlayerYears())
-		$BackupFormulations.text = "Backup Formulations: " + str($"../Player".getPlayerBackups())
-		$PlayerTurn.text = "Player " + str(currentPlayer) +"'s Turn!"
+	currentPlayerNumber = $"../PlayerTracker".getCurrentPlayer()
+	
+	currentPlayer = get_node("../Player" + str(currentPlayerNumber))
+	
+	if(currentPlayer != null):
+		$Money.text = "Money: $" + addCommas(currentPlayer.getPlayerMoney())
+		$Years.text = "Years left: " + str(currentPlayer.getPlayerYears())
+		$BackupFormulations.text = "Backup Formulations: " + str(currentPlayer.getPlayerBackups())
+		$PlayerTurn.text = "Player " + str(currentPlayerNumber) +"'s Turn!"
 	else:
 		_showAll(false)
 		_showWarning("Warning! No players found in the scene.")
