8217b899e818af18b378ef5b69c7ee144b62b03e
Tahmid Hasan <tahmidhasan1999@gmail.com>
2020-07-23 16:46:01 -0400

    Tiles integrated with Dialog Boxes
    Tiles working with dialog boxes, now its just a matter of getting the right dialog boxes to show

---
+++ Long Road to Drug Discovery/Scripts/Tile.gd
@@ -0,0 +1,182 @@
+extends Area2D
+
+export var tileNumber = 1
+export (int) var LandOnCost = -1000
+export (int) var LandOnTime = -1
+export (NodePath) var NextTilePath
+export (NodePath) var LandOnLevelPath
+var NextTile
+var LandOnLevel
+
+enum tiletype {START, GOOD, BAD, NEUTRAL}
+export (tiletype) var TileType = tiletype.START
+
+signal transfer_phaseandroll
+
+
+
+func _ready():
+	NextTile = get_node(NextTilePath)
+	LandOnLevel = get_node(LandOnLevelPath)
+	var LevelProgression = 0
+	if(LandOnLevel != null):
+		LevelProgression = LandOnLevel.getLevelNumber() - get_parent().getLevelNumber()
+	
+	if(TileType == tiletype.GOOD or TileType == tiletype.NEUTRAL or TileType == tiletype.START):
+		if(TileType == tiletype.GOOD):
+			$Sprite.texture = load("res://Custom Assets/TileSprites/shipGreen.png")
+		elif(TileType == tiletype.BAD):
+			$Sprite.texture = load("res://Custom Assets/TileSprites/shipPink.png")
+		elif(TileType == tiletype.NEUTRAL):
+			$Sprite.texture = load("res://Custom Assets/TileSprites/shipBeige.png")
+		else:
+			$Sprite.texture = load("res://Custom Assets/TileSprites/shipBlue.png")
+	else:
+		if(TileType == tiletype.GOOD):
+			$Sprite.texture = load("res://Custom Assets/TileSprites/laserGreen3.png")
+		elif(TileType == tiletype.BAD):
+			$Sprite.texture = load("res://Custom Assets/TileSprites/laserPink3.png")
+		elif(TileType == tiletype.NEUTRAL):
+			$Sprite.texture = load("res://Custom Assets/TileSprites/laserBeige3.png")
+		else:
+			$Sprite.texture = load("res://Custom Assets/TileSprites/laserBlue3.png")
+
+func LandOn(player):
+	
+	#Pause for 2 seconds- add animation
+	var t = Timer.new()
+	t.set_wait_time(2)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	yield(t, "timeout")
+	
+	updateDialogue(player,1)
+	
+	#Alter money and years
+	player.alterPlayerMoney(LandOnCost)
+	player.alterPlayerYears(LandOnTime)
+
+	if(LandOnLevel.getLevelNumber() == 1):
+		if(TileType == tiletype.GOOD or TileType == tiletype.NEUTRAL):
+			levelUp(player)
+		else:
+			tryAgain(player)
+			
+	if(LandOnLevel.getLevelNumber() == 2):
+		if(TileType == tiletype.GOOD or TileType == tiletype.NEUTRAL):
+			#INSERT CODE FOR PURCHASING BACK-UP FORMULATIONS
+			levelUp(player)
+		else:
+			tryAgain(player)
+			
+	if(LandOnLevel.getLevelNumber() == 3):
+		if(TileType == tiletype.GOOD or TileType == tiletype.NEUTRAL):
+			levelUp(player)
+		else:
+			tryAgain(player)
+			
+	if(LandOnLevel.getLevelNumber() == 4):
+		if(TileType == tiletype.GOOD or TileType == tiletype.NEUTRAL):
+			levelUp(player)
+		else:
+			tryAgain(player)
+			
+	if(LandOnLevel.getLevelNumber() == 5):
+		if(TileType == tiletype.GOOD or TileType == tiletype.NEUTRAL):
+			levelUp(player)
+		else:
+			tryAgain(player)
+			
+	if(LandOnLevel.getLevelNumber() == 6):
+		if(TileType == tiletype.GOOD or TileType == tiletype.NEUTRAL):
+			levelUp(player)
+		else:
+			tryAgain(player)
+			
+	if(LandOnLevel.getLevelNumber() == 7):
+		if(TileType == tiletype.GOOD or TileType == tiletype.NEUTRAL):
+			levelUp(player)
+		else:
+			tryAgain(player)
+			
+	if(LandOnLevel.getLevelNumber() == 8):
+		if(TileType == tiletype.GOOD or TileType == tiletype.NEUTRAL):
+			levelUp(player)
+		else:
+			tryAgain(player)
+			
+	if(LandOnLevel.getLevelNumber() == 9):
+		if(TileType == tiletype.GOOD or TileType == tiletype.NEUTRAL):
+			levelUp(player)
+		else:
+			tryAgain(player)
+		
+
+func updateDialogue(player,num):
+	var dialogue = get_tree().get_root().find_node("Dialogue",true,false)
+	connect("transfer_phaseandroll", dialogue, "_on_transfer_phaseandroll")
+	if num == 0:
+		emit_signal("transfer_phaseandroll", int(player.getCurrentLevel()), 0)
+	else:
+		emit_signal("transfer_phaseandroll", int(player.getCurrentLevel()), player.getCurrentTile())
+		
+		
+	 
+
+func levelUp(player):
+	updateDialogue(player,0)
+	
+	var t = Timer.new()
+	t.set_wait_time(2)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	yield(t, "timeout")
+	
+	var starting = LandOnLevel.getStartingTile()
+	starting.set_piece(player)
+	player.alterCurrentLevel(1)
+	get_node("../../ScrollingCamera").SetActiveLevelNumber(player.getCurrentLevel())
+	
+
+	
+func tryAgain(player):
+	updateDialogue(player,0)
+	
+	var t = Timer.new()
+	t.set_wait_time(2)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	yield(t, "timeout")
+	
+	var starting = get_parent().getStartingTile()
+	starting.set_piece(player)
+	
+
+	
+
+func GoToNextTile(player):
+	if(NextTile != null):
+		player.setMovementTarget(NextTile._getTilePosition() + Vector2(0,-50))
+		
+func StopPlayer(player):
+	player.setMovementTarget(self.position) #set target with offset
+	
+func _on_Tile_player_entered(player):
+	if(player.getCurrentTile() == tileNumber):
+		if(TileType == tiletype.START):
+			StopPlayer(player)
+		else:
+			LandOn(player)
+	else:
+		GoToNextTile(player)
+
+#The tile's position, with the level position accounted for.
+func _getTilePosition():
+	return self.get_position() + self.get_parent().get_position()
+
+func set_piece(player) -> void:
+	player.position = _getTilePosition() + Vector2(0,-50)
+	GoToNextTile(player)
